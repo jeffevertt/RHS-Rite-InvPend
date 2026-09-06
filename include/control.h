811ec608a3920833f46014ca93bdff6f5e3a1810
jevertt <jeff@evertt.com>
2026-09-05 18:15:55 -0700

    control setting tweaks

--- include/control.h
+++ include/control.h
@@ -46,15 +46,15 @@
 #define STABILIZE_LQR_GIVE_UP_ANG_VEL           0.075f                                  // angular velocity (rad/s) at which we start giving up
 #define STABILIZE_LQR_GIVE_UP_GAIN              0.5f                                    // scaled by micro-sec deltaTime & signal above max
 
-#define SWINGUP_MINIMUM_ENERGY                  2.05f
+#define SWINGUP_MINIMUM_ENERGY                  2.15f
 #define SWINGUP_TARGET_ENERGY                   (SWINGUP_MINIMUM_ENERGY + 0.1f)
 #define SWINGUP_PUMP_KICK_MM                    12.0f
 #define SWINGUP_ENERGY_GAIN                     2.5f                                    // higher is faster swingup
-#define SWINGUP_RAMP_DOWN_GAIN                  1.8f                                    // slows approach to target energy
+#define SWINGUP_RAMP_DOWN_GAIN                  2.3f                                    // slows approach to target energy
 #define SWINGUP_OUTPUT_SMOOTHING                0.1f                                    // 0.0f none, 0.9f lots of smoothing
 
 #define SWINGUP_SETUP_DELTA_SMOOTHING           0.2f                                   // 0 to 1 (0 is no smoothing)
-#define SWINGUP_SETUP_CATCH_GAIN(ENERGY)        (constrain(((ENERGY)-1.975f)*30.0f, 0.0f, 2.0f))
+#define SWINGUP_SETUP_CATCH_GAIN(ENERGY)        (constrain(((ENERGY)-1.975f)*20.0f, 0.0f, 2.0f))
 
 // macros
 #define D2R(deg)                                ((deg) * DEG_TO_RAD)
